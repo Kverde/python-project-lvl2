5f74b56189aafda292a9b29fcaac9f15cee42706
Kverde <kverde@yandex.ru>
2022-05-12 20:53:16 +0500

    step 6. tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -3,28 +3,55 @@ import pytest
 from gendiff.diff import generate_diff
 
 
-FILE1_JSON_PATH = './tests/fixtures/file1.json'
-FILE2_JSON_PATH = './tests/fixtures/file2.json'
+FILE1_JSON_PLAIN_PATH = './tests/fixtures/plain/file1.json'
+FILE2_JSON_PLAIN_PATH = './tests/fixtures/plain/file2.json'
 
-FILE1_YAML_PATH = './tests/fixtures/file1.yml'
-FILE2_YAML_PATH = './tests/fixtures/file2.yml'
+FILE1_YAML_PLAIN_PATH = './tests/fixtures/plain/file1.yml'
+FILE2_YAML_PLAIN_PATH = './tests/fixtures/plain/file2.yml'
 
-RESULT_PATH = './tests/fixtures/result.txt'
+RESULT_PLAIN_PATH = './tests/fixtures/plain/result.txt'
+
+
+FILE1_JSON_TREE_PATH = './tests/fixtures/tree/file1.json'
+FILE2_JSON_TREE_PATH = './tests/fixtures/tree/file2.json'
+
+FILE1_YAML_TREE_PATH = './tests/fixtures/tree/file1.yml'
+FILE2_YAML_TREE_PATH = './tests/fixtures/tree/file2.yml'
+
+RESULT_TREE_PATH = './tests/fixtures/tree/result.txt'
 
 
 @pytest.fixture
-def correct_result():
-    with open(RESULT_PATH) as f:
+def correct_pain_result():
+    with open(RESULT_PLAIN_PATH) as f:
         return f.read()
 
 
-def test_generate_diff_json(correct_result):
-    actual_result = generate_diff(FILE1_JSON_PATH, FILE2_JSON_PATH)
+def test_generate_diff_plain_json(correct_pain_result):
+    actual_result = generate_diff(FILE1_JSON_PLAIN_PATH, FILE2_JSON_PLAIN_PATH)
 
-    assert actual_result == correct_result
+    assert actual_result == correct_pain_result
 
 
-def test_generate_diff_yaml(correct_result):
-    actual_result = generate_diff(FILE1_YAML_PATH, FILE2_YAML_PATH)
+def test_generate_diff_plain_yaml(correct_pain_result):
+    actual_result = generate_diff(FILE1_YAML_PLAIN_PATH, FILE2_YAML_PLAIN_PATH)
 
-    assert actual_result == correct_result
+    assert actual_result == correct_pain_result
+
+
+@pytest.fixture
+def correct_tree_result():
+    with open(RESULT_TREE_PATH) as f:
+        return f.read()
+
+
+def test_generate_diff_tree_json(correct_tree_result):
+    actual_result = generate_diff(FILE1_JSON_TREE_PATH, FILE2_JSON_TREE_PATH)
+
+    assert actual_result == correct_tree_result
+
+
+def test_generate_diff_tree_yaml(correct_tree_result):
+    actual_result = generate_diff(FILE1_YAML_TREE_PATH, FILE2_YAML_TREE_PATH)
+
+    assert actual_result == correct_tree_result
